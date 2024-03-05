f3830751ca328df30a70fb80528c9c6d81cb6aa5
Rico Gunawan <ricogunawan12323@gmail.com>
2024-03-05 21:15:52 +0700

    Config Updated

--- actions/actions.py
+++ actions/actions.py
@@ -8,11 +8,15 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 #
 #
+nltk.download('stopwords')
 class ActionTellBestProduct(Action):
 
     def name(self) -> Text:
@@ -35,7 +39,17 @@ class ActionTellProduct(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # tracker.latest_message.get('text')
-        print(tracker.latest_message.get('text'))
+        # print(tracker.latest_message.get('text'))
+        last_user_message = tracker.latest_message.get('text')
+
+        tokens = word_tokenize(last_user_message)
+
+        stop_words = set(stopwords.words('english'))
+        cleaned_tokens = [word for word in tokens if word.lower() not in stop_words]
+
+        cleaned_message = ' '.join(cleaned_tokens)
+
+        print(cleaned_message)
 
         dispatcher.utter_message(text="Here is what do you need in our store:\nhttps://buybox.com/product=\"Red Shoe\"")
 
